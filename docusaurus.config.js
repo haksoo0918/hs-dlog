dc06979e3bd727ba201b9d1eb852c158f5a5678d
haksoo <haksoo0918@gmail.com>
2024-11-07 16:08:25 +0900

    설정: 문서 수정일 보이게 바꿈

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,6 +44,7 @@ const config = {
       ({
         docs: {
           sidebarPath: './sidebars.js',
+          showLastUpdateTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl:
